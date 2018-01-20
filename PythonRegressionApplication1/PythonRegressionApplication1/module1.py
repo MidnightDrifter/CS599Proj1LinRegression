7e3779d732109418e9b3f3b04b3f918d9080eed5
MidnightDrifter <sean.higgins@digipen.edu>
2018-01-19 23:55:56 -0800

    should read in the data to multiple DataFrames, whee

--- PythonRegressionApplication1/PythonRegressionApplication1/module1.py
+++ PythonRegressionApplication1/PythonRegressionApplication1/module1.py
@@ -3,3 +3,15 @@ import matplotlib
 import pandas
 
 
+trainingDataCSVPath = r'.\CSV Data Files\Redfin Data Cleaned - Choose Beds-I Bathrooms-J SqFt-L Price-H.csv'
+testingDataCSVPath = r'.\CSV Data Files\Redfin Data Cleaned - Testing Data DO NOT USE TO TRAIN.csv'
+
+colsToReadData = [9,10,12]
+colToReadPrice = [8]
+
+
+trainingDataInput = pandas.read_csv(trainingDataCSVPath, usecols=colsToReadData)
+trainingDataPrices = pandas.read_csv(trainingDataCSVPath,usecols=colToReadPrice)
+
+testingDataInput = pandas.read_csv(testingDataCSVPath,usecols=colsToReadData)
+testingDataPrices = pandas.read_csv(testingDataCSVPath,usecols=colToReadPrice)
