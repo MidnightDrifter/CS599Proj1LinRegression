769169fb51d26358acd1aaf5530facf05d6fce07
MidnightDrifter <linkmasterchief@aol.com>
2018-02-23 15:34:14 -0800

    most of the annoying file I/O stuff done I think maybe for reading in the training data at least, gotta e-mail and ask to figure out what exactly we should do with the test data

--- PythonRegressionApplication1/PythonRegressionApplication1/module1.py
+++ PythonRegressionApplication1/PythonRegressionApplication1/module1.py
@@ -3,14 +3,20 @@ import matplotlib
 import pandas
 
 #Filepaths for data files
-trainingDataCSVPath = "CSV Data Files\Redfin Data Cleaned - Choose Beds-I Bathrooms-J SqFt-L Price-H.csv"
-testingDataCSVPath = "CSV Data Files\Redfin Data Cleaned - Testing Data DO NOT USE TO TRAIN.csv"
+trainingDataCSVPath = "Proj 3 Spam-Ham Data\preprocdata\train-features-400.csv"
+testingDataCSVPath = "Proj 3 Spam-Ham Data\preprocdata\test-features.csv"
+
+#Data format:
+# Message #, Word Counter in Dictionary, # of Occurrences of Word, Spam/Ham (1=spam)
+
+ 
+
 
 #Columns of data to read in
-colsToReadData = [9,10,12]
-colToReadPrice = [8]
+#colsToReadData = [9,10,12]
+#colToReadPrice = [8]
 
-colsToSqFtVsData = [8,12]
+#colsToSqFtVsData = [8,12]
 
 #Price column: 8
 
@@ -18,13 +24,13 @@ colsToSqFtVsData = [8,12]
 #For future reference:  dataframe.shape()   gives a tuple with dimensionality of the dataframe:  (# rows, # cols)
 
 #Training & testing data
-trainingDataInput = pandas.DataFrame(pandas.read_csv(trainingDataCSVPath, usecols=colsToReadData))
-trainingDataPrices = pandas.DataFrame(pandas.read_csv(trainingDataCSVPath,usecols=colToReadPrice))
+trainingDataInput = pandas.DataFrame(pandas.read_csv(trainingDataCSVPath))
+#trainingDataPrices = pandas.DataFrame(pandas.read_csv(trainingDataCSVPath,usecols=colToReadPrice))
 
-testingDataInput = pandas.DataFrame(pandas.read_csv(testingDataCSVPath,usecols=colsToReadData))
-testingDataPrices = pandas.DataFrame(pandas.read_csv(testingDataCSVPath,usecols=colToReadPrice))
+testingDataInput = pandas.DataFrame(pandas.read_csv(testingDataCSVPath))
+#testingDataPrices = pandas.DataFrame(pandas.read_csv(testingDataCSVPath,usecols=colToReadPrice))
 
-testingPricesVsSqFt = pandas.DataFrame(pandas.read_csv(testingDataCSVPath, usecols=colsToSqFtVsData))
+#testingPricesVsSqFt = pandas.DataFrame(pandas.read_csv(testingDataCSVPath, usecols=colsToSqFtVsData))
 
 #Size of data
 trainingDataLength = 0
@@ -37,40 +43,43 @@ if(trainingDataLength == 0 or testingDataLength ==0):
     print("Something has gone wrong.")
 
 
+numTrainingEmails = trainingDataInput["Message #"].max()
+numTestingEmails = testingDataInput["Message #"].max()
 
 
-#Append all 1's column to left
-trainingDataInput.insert(0, 'Ones', 1)
-testingDataInput.insert(0,'Ones',1)
+trainingDataSpamDictionary = {}
+trainingDataHamDictionary = {}
+testingDataSpamDictionary = {}
+testingDataHamDictionary = {}
 
 
-
-#Turn into matrices here
-trainingDataMatrix = numpy.matrix(trainingDataInput.values)
-trainingDataMatrixTranspose = trainingDataMatrix.transpose
-trainingDataPricesMatrix = numpy.matrix(trainingDataPrices.values)
-
-trainingDataMatrixInv = (trainingDataMatrixTranspose * trainingDataMatrix).inverse
-
-testingDataMatrix = numpy.matrix(testingDataInput.values)
-testingDataPricesMatrix = numpy.matrix(testingDataPrices.values)
-
-#Multiply here
-thetaValues = trainingDataMatrixInv * trainingDataMatrixTranspose * trainingDataPricesMatrix
-
-#Graph here
-
-predictedPrices = testingDataMatrix * thetaValues
+#Kinda cheat-y, but know that:  A. training data has 400 emails & B. testing data has 260
 
 
 
-matplotlib.pyplot.plot(predictedPrices,predictedPrices)
-matplotlib.pyplot.plot(numpy.matrix(testingPricesVsSqFt.values))
-
-matplotlib.pyplot.xlabel("Prices")
-matplotlib.pyplot.ylabel("Sq. Ft.")
-matplotlib.pyplot.show()
 
 
+for index, row in trainingDataInput.itertuples():
+    if(row[3] ==0):
+        if(row[1] not in trainingDataHamDictionary):
+            trainingDataHamDictionary[row[1]] = row[2]
+        else:
+            trainingDataHamDictionary[row[1]] += row[2]
+    else:
+        if(row[1] not in trainingDataSpamDictionary):
+            trainingDataSpamDictionary[row[1]] = row[2]
+        else:
+            trainingDataSpamDictionary[row[1]] += row[2]
 
-#Graph stuff goes here??
+
+#for index, row in testingDataInput.itertuples():
+#    if(row[3] ==0):
+#        if(row[1] not in testingDataHamDictionary):
+#            testingDataHamDictionary[row[1]] = row[2]
+#        else:
+#            testingDataHamDictionary[row[1]] += row[2]
+#    else:
+#        if(row[1] not in testingDataSpamDictionary):
+#            testingDataSpamDictionary[row[1]] = row[2]
+#        else:
+#            testingDataSpamDictionary[row[1]] += row[2]
