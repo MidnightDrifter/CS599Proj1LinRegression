deefdc67fff2aa548c3c11439f3b1572a2f8bf61
MidnightDrifter <linkmasterchief@aol.com>
2018-04-16 23:04:39 -0700

    Initial commit, some commenting set-up for the neural net

--- PythonRegressionApplication1/PythonRegressionApplication1/regression.py
+++ PythonRegressionApplication1/PythonRegressionApplication1/regression.py
@@ -55,131 +55,13 @@ testingDataLength = 0
 trainingDataLength = len(trainingDataInput.index)
 testingDataLength = len(testingDataInput.index)
 
-#if(trainingDataLength == 0 or testingDataLength ==0):
-#    print("Something has gone wrong.")
-E = math.e
-PI = math.pi
-NUM_CLUSTERS = 2
+#Input matrix - 4x3
+
+#XOR:  x , y, x XOR y
+#[ 0  0  0]
+#[ 0  1  1]
+#[ 1  0  1]
+#[ 1  1  0]
 
 
-
-NUM_DATA_POINTS = trainingDataLength
-
-
-#If this doesn't work, just store them in separate arrays for sanity's sake
-#clusterSizes = [125] * NUM_CLUSTERS  #Initial cluster size = 125
-#clusterCentersX = [0.0] * NUM_CLUSTERS   #Vi x-coord
-
-#don't use this
-#dataPointClusterAssignment = [0] * NUM_CLUSTERS   #Set the first 125 to 1st cluster (index 0), last 125 to 2nd cluster (index 1)
-
-
-#DO use this
-sigmaVals = [1]*NUM_CLUSTERS
-meanVals = [1]*NUM_CLUSTERS
-phiVals = [1.0/NUM_CLUSTERS] * NUM_CLUSTERS
-WVals = numpy.ones((NUM_DATA_POINTS,  NUM_CLUSTERS))   #num rows, num cols  ==>   index, cluster num
-
-MAX_X_VAL = trainingDataInput["eruptions"].max()
-MIN_X_VAL = trainingDataInput["eruptions"].min()
-
-MAX_LOOPS = 1000
-
-meanVals[0] = random(MIN_X_VAL,MAX_X_VAL)
-
-meanVals[1] = random(MIN_X_VAL,MAX_X_VAL)
-while(meanVals[1] == meanVals[0]):
-    meanVals[1] = random(MIN_X_VAL,MAX_X_VAL)
-
-
-
-
-#Set cluster centers--avg of first, second 125 inputs
-##Don't use this at all
-#for row, index in trainingDataInput.iterrows():
-#    if(rows >0):
-#        if(rows <126):
-#            clusterCentersX[0] += index[1]   #.values
-#            #clusterCentersY[0] += index[2]   #.values
-#        else:
-#            clusterCentersX[1] += index[2]   #.values
-#            #clusterCentersY[1] += index[2]   #.values
-#            dataPointClustersAssignment[row] =1
-
-#Don't use this at all
-#def UpdateClusterCenters():
-#    for i in range(0,NUM_CLUSTERS):
-#        clusterCenters[i] /= float(clusterSizes[i])
-#        #clusterCentersX[i] = tuple(x / float(clusterSizes[i]) for x in clusterCentersX[i])
-#        #clusterCentersY[i] = tuple(x / float(clusterSizes[i]) for x in clusterCentersY[i])
-
-
-def GetXVal(i):
-    return trainingDataInput.iloc[[i+1][1]]
-
-def GetYVal(i):
-    return trainingDataInput.iloc[[i+1][2]]
-
-#def UpdateClusterSizes():
-#    clusterSizes[0]=0
-#    clusterSizes[1]=0
-#    for(i in range(0, NUM_DATA_POINTS)):
-#        if(WVals[i][0] > WVals[i][1]):
-#            clusterSizes[0] +=1
-#        else:
-#            clusterSizes[1] +=1
-
-# P(Xi | Zj)
-def MembershipGrade(trainingDataIndex, meanIndex, sigmaIndex):
-    return (  (1/(math.sqrt(2*PI)*sigmaVals[sigmaIndex]) ) * ( math.pow(E, (math.pow( ( GetXVal(trainingDataIndex) - meanVals[meanIndex]), 2  )) / ( -2 * math.pow(sigmaVals[sigmaIndex],2) )  )  ) )
-
-
-
-def UpdateWValues():
-    for i in range(0,NUM_DATA_POINTS):
-        for j in range(0,NUM_CLUSTERS):
-            sum =0.0
-            myGrade = MembershipGrade(i,j,j) * phiVals[j]
-            for l in range(0,NUM_DATA_POINTS):
-                sum += MembershipGrade(i,l,l) * phiVals[l]
-            WVals[i][j] = myGrade/sum
-
-
-def UpdateMeanValues():
-    for j in range(0,NUM_CLUSTERS):
-        wSum =0.0
-        wProduct =0.0
-        for i in range(0,NUM_DATA_POINTS):
-            wSum += WVals[i][j]
-            wProduct += WVals[i][j] * GetXVal(i)
-        meanVals[j] = wSum / wProduct
-
-def UpdatePhiValues():
-    for j in range(0,NUM_CLUSTERS):
-        wSum =0.0
-        for i in range(0,NUM_DATA_POINTS):
-            wSum += WVals[i][j]
-        phiVals[j] = wSum / NUM_DATA_POINTS
-
-
-def UpdateSigmaValues():
-    for j in range(0,NUM_CLUSTERS):
-        wSum=0.0
-        wProduct=0.0
-        for i in range(0,NUM_DATA_POINTS):
-            wSum += WVals[i][j]
-            wProduct += WVals[i][j] * math.pow((GetXVal(i) - meanVals[j]), 2)
-        sigmaVals[j]=wProduct/wSum
-
-
-for b in range(0,MAX_LOOPS):
-    UpdateWValues()
-
-    
-    UpdatePhiValues()
-    UpdateMeanValues()
-    UpdateSigmaValues()
-
-
-#Should have normal distributions at this point--what do we do with them?
-
+#Division:   x, y, x/y   where x & y are in [1, 10], equidistant  (???)
